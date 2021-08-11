87f6fe8d218e45e08823b4ae93ed2f18d3975372
beatrixcsaki <48450860+beatrixcsaki@users.noreply.github.com>
2021-08-11 18:57:32 +0300

    First commit

---
+++ WeatherApp/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿
+namespace WeatherApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtcity = new System.Windows.Forms.TextBox();
+            this.txtwindk = new System.Windows.Forms.TextBox();
+            this.txthumidity = new System.Windows.Forms.TextBox();
+            this.txtrainfall = new System.Windows.Forms.TextBox();
+            this.txtfeelslike = new System.Windows.Forms.TextBox();
+            this.txttemp = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Cloud = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "City";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 82);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Temperature";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 127);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Feels like";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 175);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Rainfall";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(56, 221);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Wind Speed kph";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(56, 266);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Humidity";
+            // 
+            // txtcity
+            // 
+            this.txtcity.Location = new System.Drawing.Point(229, 37);
+            this.txtcity.Margin = new System.Windows.Forms.Padding(4);
+            this.txtcity.Name = "txtcity";
+            this.txtcity.Size = new System.Drawing.Size(132, 22);
+            this.txtcity.TabIndex = 6;
+            // 
+            // txtwindk
+            // 
+            this.txtwindk.Location = new System.Drawing.Point(229, 221);
+            this.txtwindk.Margin = new System.Windows.Forms.Padding(4);
+            this.txtwindk.Name = "txtwindk";
+            this.txtwindk.Size = new System.Drawing.Size(132, 22);
+            this.txtwindk.TabIndex = 7;
+            // 
+            // txthumidity
+            // 
+            this.txthumidity.Location = new System.Drawing.Point(229, 266);
+            this.txthumidity.Margin = new System.Windows.Forms.Padding(4);
+            this.txthumidity.Name = "txthumidity";
+            this.txthumidity.Size = new System.Drawing.Size(132, 22);
+            this.txthumidity.TabIndex = 8;
+            // 
+            // txtrainfall
+            // 
+            this.txtrainfall.Location = new System.Drawing.Point(229, 172);
+            this.txtrainfall.Margin = new System.Windows.Forms.Padding(4);
+            this.txtrainfall.Name = "txtrainfall";
+            this.txtrainfall.Size = new System.Drawing.Size(132, 22);
+            this.txtrainfall.TabIndex = 9;
+            // 
+            // txtfeelslike
+            // 
+            this.txtfeelslike.Location = new System.Drawing.Point(229, 127);
+            this.txtfeelslike.Margin = new System.Windows.Forms.Padding(4);
+            this.txtfeelslike.Name = "txtfeelslike";
+            this.txtfeelslike.Size = new System.Drawing.Size(132, 22);
+            this.txtfeelslike.TabIndex = 10;
+            // 
+            // txttemp
+            // 
+            this.txttemp.Location = new System.Drawing.Point(229, 82);
+            this.txttemp.Margin = new System.Windows.Forms.Padding(4);
+            this.txttemp.Name = "txttemp";
+            this.txttemp.Size = new System.Drawing.Size(132, 22);
+            this.txttemp.TabIndex = 11;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(462, 158);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(177, 97);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Cloud
+            // 
+            this.Cloud.AutoSize = true;
+            this.Cloud.Location = new System.Drawing.Point(476, 137);
+            this.Cloud.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Cloud.Name = "Cloud";
+            this.Cloud.Size = new System.Drawing.Size(44, 17);
+            this.Cloud.TabIndex = 13;
+            this.Cloud.Text = "Cloud";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(448, 31);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 35);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Show current weather";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 439);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(937, 166);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 393);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(235, 38);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Show weather for the next 2 days";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(490, 408);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Export";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(395, 409);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(392, 388);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(173, 17);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Enter directory path below";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1125, 693);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Cloud);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txttemp);
+            this.Controls.Add(this.txtfeelslike);
+            this.Controls.Add(this.txtrainfall);
+            this.Controls.Add(this.txthumidity);
+            this.Controls.Add(this.txtwindk);
+            this.Controls.Add(this.txtcity);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "WeatherApp";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtcity;
+        private System.Windows.Forms.TextBox txtwindk;
+        private System.Windows.Forms.TextBox txthumidity;
+        private System.Windows.Forms.TextBox txtrainfall;
+        private System.Windows.Forms.TextBox txtfeelslike;
+        private System.Windows.Forms.TextBox txttemp;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Cloud;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WeatherApp/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+
+namespace WeatherApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string path;
+        string city;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            city = txtcity.Text;
+            string uri = string.Format("http://api.weatherapi.com/v1/current.xml?key=4657ab980733494080b153623210904&q={0}&aqi=no", city);
+            XDocument doc = XDocument.Load(uri);
+            String iconUri = (string)doc.Descendants("icon").FirstOrDefault();
+            WebClient client = new WebClient();
+            byte[] image = client.DownloadData("http:" + iconUri);
+            MemoryStream stream = new MemoryStream(image);
+
+            Bitmap newBitMap = new Bitmap(stream);
+            string temp = (string)doc.Descendants("temp_c").FirstOrDefault();
+            string feelslike = (string)doc.Descendants("feelslike_c").FirstOrDefault();
+            string rainfall = (string)doc.Descendants("precip_mm").FirstOrDefault();
+            string wind = (string)doc.Descendants("wind_kph").FirstOrDefault();
+            string humidity = (string)doc.Descendants("humidity").FirstOrDefault();
+
+            string country = (string)doc.Descendants("country").FirstOrDefault();
+            string cloud = (string)doc.Descendants("text").FirstOrDefault();
+
+            Bitmap icon = newBitMap;
+            txttemp.Text = temp;
+            txtfeelslike.Text = feelslike;
+            txtrainfall.Text = rainfall;
+            txtwindk.Text = wind;
+            txthumidity.Text = humidity;
+
+            pictureBox1.Image = icon;
+
+        }
+
+        //private void getweather(string city) {}
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Region", typeof(string));
+            dt.Columns.Add("Date", typeof(string));
+            dt.Columns.Add("Max Temperature", typeof(string));
+            dt.Columns.Add("Min Temperature", typeof(string));
+            dt.Columns.Add("Rainfall", typeof(string));
+            dt.Columns.Add("Wind speed kph", typeof(string));
+            dt.Columns.Add("Humidity", typeof(string));
+            dt.Columns.Add("Cloud", typeof(string));
+            dt.Columns.Add("Icon", typeof(Bitmap));
+
+            city = txtcity.Text;
+            Console.WriteLine(city);
+
+            string uri = string.Format("https://api.weatherapi.com/v1/forecast.xml?key=4657ab980733494080b153623210904&q={0}&days=7", city);
+            XDocument doc = XDocument.Load(uri);
+
+            foreach (var npc in doc.Descendants("forecastday"))
+            {
+                Console.WriteLine(1);
+                //day = npc.Descendants("forecastday").FirstOrDefault();
+                string iconUri = (string)npc.Descendants("icon").FirstOrDefault();
+                WebClient client = new WebClient();
+                byte[] image = client.DownloadData("http:" + iconUri);
+                MemoryStream stream = new MemoryStream(image);
+
+                Bitmap newBitmap = new Bitmap(stream);
+
+
+                dt.Rows.Add(new object[]
+                {
+                   (string)doc.Descendants("region").FirstOrDefault(),
+                   (string)npc.Descendants("date").FirstOrDefault(),
+
+                   (string)npc.Descendants("maxtemp_c").FirstOrDefault(),
+                   (string)npc.Descendants("mintemp_c").FirstOrDefault(),
+                   (string)npc.Descendants("totalprecip_mm").FirstOrDefault(),
+                   (string)npc.Descendants("avgvis_km").FirstOrDefault(),
+                   (string)npc.Descendants("avghumidity").FirstOrDefault(),
+
+                   (string)npc.Descendants("text").FirstOrDefault(),
+                   newBitmap
+
+
+                });
+            }
+            dataGridView1.DataSource = dt;
+
+        }
+
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            path=textBox1.Text;
+            city = txtcity.Text;
+            string uri = string.Format("https://api.weatherapi.com/v1/forecast.xml?key=4657ab980733494080b153623210904&q={0}&days=7", city);
+            XDocument doc = XDocument.Load(uri);
+            doc.Save(path + "Weather.xml");
+        }
+
+    }
+  
+}
